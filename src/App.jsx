708bac7dd989d170e62ecaa7702f5568364bf637
Kevin Yabut <156222818+KevinYabut08@users.noreply.github.com>
2025-09-29 14:08:32 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <body className="bg-black text-center">
+    <div>
+      <h1 className="text-white">Hello Campst Collective Ltd</h1>
+    </div>   
+    </body>
+  )
+}
+
+export default App

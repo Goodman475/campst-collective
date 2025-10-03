44815bb7fcf2b68bcd2f84bb856bf81e1360be06
Kevin Yabut <156222818+KevinYabut08@users.noreply.github.com>
2025-10-03 08:37:17 -0600

    fixed some positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,19 +3,19 @@ import heroIcon from '../assets/hero-bg.jpg';
 
 const Hero = () => {
   return (
-    <div className="relative text-lime-900 pt-10 text-center w-full h-170 flex flex-col justify-start items-start">
+    <div className="relative text-lime-900 pt-10 text-center w-full h-150 md:h-170 flex flex-col justify-start items-center md:items-start">
       <img
         src={heroIcon}
         alt="hero-img"
         className="absolute inset-0 w-full h-full object-cover z-0 opacity-90"
       />
       <h1
-        className="relative text-9xl mt-20 mb-auto text-lime-950 font-extrabold drop-shadow-lg"
+        className="relative text-5xl md:text-9xl mt-20 mb-0 md:mb-auto text-lime-950 font-extrabold drop-shadow-lg"
       >
         CAMPST 
       </h1>
         <h1
-        className="relative text-9xl mb-100 text-lime-950 font-extrabold drop-shadow-lg"
+        className="relative text-5xl md:text-9xl mb-100 text-lime-950 font-extrabold drop-shadow-lg"
       >
         COLLECTIVE
       </h1>
